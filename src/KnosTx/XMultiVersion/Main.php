3909c4dfb7dfa89eaa0c28912cbc464b64fe4f76
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-10-13 18:59:49 +0700

    Update Main.php

--- src/KnosTx/XMultiVersion/Main.php
+++ src/KnosTx/XMultiVersion/Main.php
@@ -15,7 +15,7 @@ use pocketmine\utils\Config;
 class Main extends PluginBase implements Listener {
 
     private string $serverVersion;
-    private array $supportedProtocols = [685, 686, 700, 710, 720, 729];
+    private array $supportedProtocols = [685, 690, 685, 686, 712, 729];
     private Config $playerData;
 
     public function onEnable(): void {
