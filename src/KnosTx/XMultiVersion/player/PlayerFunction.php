5a8c687eb56fa64b951b4fcc543ef508c2fb776e
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-10-13 19:46:27 +0700

    Update PlayerFunction.php

--- src/KnosTx/XMultiVersion/player/PlayerFunction.php
+++ src/KnosTx/XMultiVersion/player/PlayerFunction.php
@@ -5,6 +5,10 @@ namespace KnosTx\XMultiVersion\player;
 use pocketmine\player\Player;
 
 class PlayerFunction extends Player {
+    public function __construct($loader, $server, $description, $dataFolder, $file, $resourceProvider) {
+        parent::__construct($loader, $server, $description, $dataFolder, $file, $resourceProvider);
+    }
+
     public function getUsername(): string {
         return $this->username;
     }
